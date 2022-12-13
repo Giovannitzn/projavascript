869cef1fcb795046049a1b32ba43b2a2a7feaca6
Giovanni Tzn <giotz2020@gmail.com>
2022-12-13 07:34:33 -0500

    nuevo concepto

--- js/logica.js
+++ js/logica.js
@@ -1,37 +1,52 @@
 
-let nombre = prompt("Para poder iniciar, Ingrese su nombre");
-let apellido = prompt("Gracias, ahora su apellido:");
+let nombre = prompt("Para poder iniciar, Ingrese su nombre").toUpperCase();
+let apellido = prompt("Gracias, ahora su apellido:").toUpperCase();
 let edad = parseInt (prompt ("Por ultimo que edad tienes:"));
 let mensaje = `Hola ${nombre} ${apellido} bienvenido, a la tienda de la zapatilla ...`;
 
 if (edad>=18) {
-    let mensaje1 = `Eres mayor de edad, puedes continuar ... `;
+    let mensaje1 = `${nombre} sabemos que eres mayor de edad , puedes continuar ... `;
+    alert(mensaje);
     alert(mensaje1);
-    let deporte = prompt("¿que tipo de deporte practicas: el caminar, correr o quiza el futbol?")
+    let deporte = prompt("¿que tipo de deporte practicas?: tennis, basquet o quiza futbol?")
     while (deporte != null){
           switch (deporte) {
-            case "caminar":
+            case "tennis":
             alert("Tenemos las zapatillas que quiza te interesen");
             break;
-            case "correr":
-                alert("En nuestra galeria encontraras las zapatillas mas comodas y livianas para esta actividad");
+            case "basquet":
+                alert("En nuestra galeria encontraras las zapatillas mas comodas y livianas para correr");
             break;
             case "futbol":
                 alert("Con la variedad de modelos disfrutaras mejor de tu pasión y tus goles");
                 break;
             default:
-                alert("Disculpa aun no tenemos zapatillas para ello");
+                alert("Disculpa aun no tenemos productos");
                  break;
             }break;
         }
-        let finde = prompt("Los zapatos quiza son los que van mejor para ti");
-            if (finde ==="si") {
-                alert("Pronto, te tendremos sorpresas");
-            } else {
-                alert("Estamos trabajando para ti");
-            }
+
+        let marcaIn = prompt("Ingrese la marca del articulo").toUpperCase();
+        let modeloIn = prompt("Ingrese el modelo del articulo").toUpperCase();
+        let precioIn = parseInt (prompt("Ingrese el precio del articulo"));
+        let cantidadIn = parseInt (prompt("Ingrese la cantidad del articulo"));
+        
+        
+        class Zapatilla{
+            constructor(marca, modelo, precio, cantidad) {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+        }
+        
+        let zapatilla1 = new Zapatilla(marcaIn, modeloIn, precioIn, cantidadIn);
+        
+        alert("La zapatilla que estas buscando es del modelo " + zapatilla1.modelo + " marca: " + zapatilla1.marca + " y tiene precio " + zapatilla1.precio);
+
 } else {
-    alert ("No se ingreso bien tu edad o eres menor de edad, gracias por tu visita");
+    alert ("Entiendo que menor de edad, o no entendi tu edad...");
 }
 
-alert("Muchas gracias por visitarnos, espero vuelvas pronto");
+alert("Gracias por visitarnos, espero vuelvas pronto");
